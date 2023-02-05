1f84c9fc88949774db5092d036bced2761bf680b
Matheus Soares Camacho <matheus_camacho27@hotmail.com>
2023-02-05 14:31:45 -0300

    a

--- src/server/api.ts
+++ src/server/api.ts
@@ -23,32 +23,32 @@ router.get("/adventurers", async (req, res) => {
   res.send("Pong");
 });
 
-router.get("/adventurer/uuid/:id", async (req, res) => {
-  const { id } = req.params;
-  if (id) {
-    const uuidRegex =
-      /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
-    if (!id.match(uuidRegex)) {
-      return res.status(400).json({ err: "UUID inválido." });
-    }
-    console.log("Chegou aq -> UUID");
-    const response = await getPlayerByUUID(id);
+router.get("/adventurer/nick/:nick", async (req, res) => {
+  const { nick } = req.params;
+  if (nick) {
+    console.log("Chegou aq -> NICK");
+    const response = await getPlayerByUUID(nick);
     res.status(200).send(response);
   }
 });
 
 router.post("/adventurer/login/", async (req, res) => {
   console.log(req.body);
-  const { id, senha } = req.body;
-  if (id && senha) {
-    const uuidRegex =
-      /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
-    if (!id.match(uuidRegex)) {
-      return res.status(400).json({ err: "UUID inválido." });
-    }
+  const { nick, senha } = req.body;
+  if (nick && senha) {
     console.log("Chegou aq -> Senha");
+  
+    const data = await getPlayerLogin(nick, senha);
+    console.log(data.toString);
 
-    const data = await getPlayerLogin(id, senha);
+    const jason = JSON.parse(data.toString());
+
+    console.log(jason);
+
+    const password = jason[0].password;
+
+    console.log(password);
+
     res.status(200).send(data);
   }
 });
--- src/services/UsersManager.ts
+++ src/services/UsersManager.ts
@@ -1,14 +1,20 @@
 import { connection } from "./MySqlConnection";
 
 export async function getPlayerByUUID(uuid: String) {
-  const query = `SELECT id, uuid, nick, displayNick, permission, email, created_at FROM adventurer WHERE uuid=?`;
+  const query = `SELECT id, uuid, nick, displayNick, permission, email, created_at FROM adventurer WHERE nick=?`;
   const [rows] = await (await connection).query(query, [uuid]);
   console.log(rows);
   return rows;
 }
 export async function getPlayerLogin(uuid: String, password: String) {
-  const query = `SELECT id, uuid, nick, displayNick, permission, email, created_at FROM adventurer WHERE uuid=? and password=?`;
+  const query = `SELECT password FROM adventurer WHERE nick=? and password=?`;
   const [rows] = await (await connection).query(query, [uuid, password]);
+  console.log(rows);
+  return rows;
+}
+export async function registerPlayer(uuid: String, nick: String, password: String, displayNick: String, permission: String, email: String) {
+  const query = `Insert into adventurer(uuid, nick, password, displayNick, permission, email) VALUES (?,?,?,?,?,?)`;
+  const [rows] = await (await connection).query(query, [uuid, nick, password, displayNick, permission, email]);
   console.log(rows);
   return rows;
 }
